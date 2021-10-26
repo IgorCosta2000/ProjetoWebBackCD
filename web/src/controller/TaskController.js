ac0c509265b0126a6f269e051982e1130477f799
IgorCosta2000 <igorcosazevedo@gmail.com>
2021-10-26 15:13:43 -0300

    Métodos realAll,readById e Delete

--- web/src/controller/TaskController.js
+++ web/src/controller/TaskController.js
@@ -34,7 +34,8 @@ class TaskController{
     }
     async update(req,res){
         
-       await TaskModel.findByIdAndUpdate({'_id' : req.params.id} ,req.body, {new:true})
+       await TaskModel
+       .findByIdAndUpdate({'_id' : req.params.id} ,req.body, {new:true})
         .then(response => {return res.status(200).json(response)})
         .catch(error => {return res.status(500).json(error)});
     
@@ -43,8 +44,48 @@ class TaskController{
     
     
     }
-       
-
+    async readAll(req,res){
+        
+        await TaskModel
+        .find({macadress: {'$in': req.body.macadress}})
+        .sort('when')
+         .then(response => {return res.status(200).json(response)})
+         .catch(error => {return res.status(500).json(error)});
+     
+     
+     
+     
+     
+     }
+     async readById(req,res){
+        
+        await TaskModel
+        .findById(req.params.id)
+         .then(response => {
+            if(response) 
+                return res.status(200).json(response)
+            else
+                return res.status(404).json({error : 'Tarefa Não Encontrada' })
+         })
+         .catch(error => {return res.status(500).json(error)});
+     
+     
+     
+     
+     
+     }
+     async delete(req,res){
+        
+        await TaskModel
+        .deleteOne({'_id' : req.params.id})
+         .then(response => {return res.status(200).json(response)})
+         .catch(error => {return res.status(500).json(error)});
+     
+     
+     
+     
+     
+     }
        }
        
 
